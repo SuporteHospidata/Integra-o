27a44bafa4083f1041abf98adf3858e5ca35af28
Willian Barros <will.santos26@yahoo.com.br>
2021-11-25 11:53:21 -0300

    COMMIT INICIAL

---
+++ view-vivace-animat.sql
@@ -0,0 +1,3 @@
+/*View para validar se as requisições estão subindo*/
+
+select * from integracao.requisicoes_integracoes_ris_pacs where acession_number in (XXXX, XXXX,);-> Setar o acession_number = Protocolo em sigh ou sigclin.
